83ce23ba10f9f3c534f27380644d8aedea91db3b
jcl86 <jorgecano86@gmail.com>
2021-04-03 21:14:51 +0200

    Navbar

---
+++ src/Celebrity.Web/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Celebrity.Web
+{
+    public class Constants
+    {
+        public const string Primary = "blue-800";
+        public const string PrimaryLighter = "blue-700";
+        public const string PrimaryDarker = "blue-900";
+
+    }
+
+}
